2a3d8aa0bb9136804386791ca2cb39d673fc216c
jg2git <f18autocad@gmail.com>
2024-03-13 19:58:39 +0100

    Added functionalities for version 2

--- includes/sensor_thermostat.h
+++ includes/sensor_thermostat.h
@@ -4,26 +4,23 @@
 * The code was developed for a coding test from Qblox.
 * License - Free to use
 *
-* Description:The file contains the declarations for the temperature sensor.
+* Description:The file contains the declarations for the thermostat temperature sensor.
 *
 ************************************************************************************************************************/
-
+#ifndef H_SENSOR_THERMAL
+#define H_SENSOR_THERMAL
+#include <sensor_thermal.h>
+#endif
 
 #include <string>
 
 namespace q_coding {
 
-const int SENSOR_MAX_TEMP = 80;                   //Default max allowable temperature
-const int MAX_NODE_ADDR_RANGE = 2000;             //Sensor device node address
-const int MAX_REG_ADDR_RANGE = 0x2FF;             //Temperature register address
-const int MIN_BAUD_RATE = 5000;                   //Device communication min baud rate
-const int MAX_BAUD_RATE = 3000000;                //Device communication max baud rate
 
-class sensor_thermostat {
+class sensor_thermostat : public sensor_thermal {
 	public:
 	sensor_thermostat();                          //default constructor
 	sensor_thermostat(std::string sensor_name);
-	sensor_thermostat(std::string sensor_name, unsigned int baud_rate, unsigned int node_address, unsigned int reg_address);
 	~sensor_thermostat();                         //default destructor
 	//Member functions of the class
 	int check_threshold(bool& alarm);             //Checks the threshold
@@ -32,32 +29,19 @@ class sensor_thermostat {
 	int setup_sensor(unsigned int node_address, unsigned int reg_address, unsigned int baud_rate);   //initialize the sensor
 	int print_param(void);                        //Print the values of the attributes of the sensor	
 	int get_sensor_name(std::string& sensor_name);//Get sensor name
-        int shutdown_sensor(void);                    //Shutdowns the sensor
+    int shutdown_sensor(void);                    //Shutdowns the sensor
 	int get_threshold(float& setpoint);           //Gets the threshold that is already set
-        int get_internal_param(	float& sensor_data, float& sensor_alarm_threshold, int& conn_state, int& error);
+    int get_internal_param(	float& sensor_data, float& sensor_alarm_threshold, int& conn_state, int& error);
 	
 	protected:
-		enum {
-		SENSOR_RESET_STATE = 0,
-		SENSOR_INITIALIZED,
-		SENSOR_CONNECTED_STATE,
-		SENSOR_DISCONNECTING_STATE,
-		SENSOR_CLEANUP_STATE,
-		SENSOR_ERROR
-	} CONN_STATE;                                //States of the sensor device
+	std::string sensor_name;                     //Name of the sensor
+	unsigned int baud_rate;                      //Communication interface baud rate
+    unsigned int node_address;                   //Communication node address
+    unsigned int reg_address;                    //Register address of the data to be read from
+	int sensor_type;                             //Sensor device type
 	
-	//Member variables of the class
-        std::string sensor_name;
-	unsigned int baud_rate;
-        unsigned int node_address;
-        unsigned int reg_address;
-	float sensor_data;
-	float alarm_threshold;
-	int conn_state;
-	int error;
-	
-	private:
 	//Members that interact with the low level code or C functions
+	private:
 	int connect(void);       //connect to the sensor
 	int reset(void);         // reset the sensor
 	int disconnect(void);    //terminate the connection
