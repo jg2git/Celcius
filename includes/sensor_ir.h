252d68a13ea64f61ad95aa45eef786f67d6dd0cf
jg2git <161865264+jg2git@users.noreply.github.com>
2024-03-14 15:17:06 +0100

    Update sensor_ir.h

--- includes/sensor_ir.h
+++ includes/sensor_ir.h
@@ -40,7 +40,7 @@ class sensor_ir : public sensor_thermal {
         unsigned int reg_address;                     //Register address of the data to be read from
 	int sensor_type;                              //Sensor device type
 	
-	//Members that interact with the low level code or C functions
+	//Calls that interact with the low level code or C functions
 	private:
 	int connect(void);       //connect to the sensor
 	int reset(void);         // reset the sensor
